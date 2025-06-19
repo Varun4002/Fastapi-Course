fd6845e754b429c1eef19ae5e2e1f0178bc316ee
Varun4002 <varunsharma6084@gmail.com>
2025-06-19 21:23:27 +0530

    Updated main.py, added 1.py, deleted old file in collection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randrange
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
@@ -10,16 +11,32 @@ class Post(BaseModel):
     published : bool =True
     rating : Optional[int]=None
 
+
+my_posts = [{"title":"title of post 1","content":"content of post 1","id":1},
+            {"title":"i love pizza","content":"hey i love pizza","id":2}]
+
+def find_Post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+
 @app.get("/")
 def root():
-    return {"message":"Welcome to root!!!"}
+    return {"message":"root dir"}
 
 @app.get("/posts")
 def get_post():
-    return {"message":"here are posts"}
+    return {"data":my_posts}
     
 @app.post("/posts")
 def create_post(post:Post):
-    print(post.published)
-    print(post.rating)
-    return {"message": post}
+    post_dict = post.dict()
+    post_dict["id"]=randrange(0,10000000)
+    my_posts.append(post_dict)
+    return {"message": my_posts}
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    print(find_Post(id))
+    return {"message":find_Post(id)}
+
